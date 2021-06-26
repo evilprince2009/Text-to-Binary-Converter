c36fea69b3c7ce65c7e6feed1bd11be7e0f2463a
Ibne Nahian <evilprince2009@gmail.com>
2021-06-26 18:44:56 +0600

    app up

---
+++ StringToBinaryConverter/AppForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace StringToBinaryConverter
+{
+    partial class AppForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AppForm));
+            this.inputText = new System.Windows.Forms.RichTextBox();
+            this.outText = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputText
+            // 
+            this.inputText.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.inputText.Location = new System.Drawing.Point(23, 83);
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(350, 130);
+            this.inputText.TabIndex = 2;
+            this.inputText.Text = "";
+            this.inputText.TextChanged += new System.EventHandler(this.inputText_TextChanged);
+            // 
+            // outText
+            // 
+            this.outText.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.outText.Location = new System.Drawing.Point(23, 238);
+            this.outText.Name = "outText";
+            this.outText.Size = new System.Drawing.Size(350, 130);
+            this.outText.TabIndex = 3;
+            this.outText.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(23, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter your text here";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(23, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Binary representation";
+            // 
+            // AppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 391);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outText);
+            this.Controls.Add(this.inputText);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "AppForm";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Yellow;
+            this.Text = "Text to Binary Converter";
+            this.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox inputText;
+        private System.Windows.Forms.RichTextBox outText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ StringToBinaryConverter/AppForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using MetroFramework.Forms;
+
+namespace StringToBinaryConverter
+{
+    public partial class AppForm : MetroForm
+    {
+        public AppForm()
+        {
+            InitializeComponent();
+        }
+
+        private void inputText_TextChanged(object sender, EventArgs e)
+        {
+            string incomingText = inputText.Text;
+            outText.Text = ToBinary(incomingText, Encoding.ASCII);
+        }
+        private string ToBinary(string text, Encoding encoding)
+        {
+            return string.Join(" ", encoding.
+                GetBytes(text)
+                .Select(t => Convert
+                    .ToString(t, 2)
+                    .PadLeft(8, '0')));
+        }
+    }
+}
